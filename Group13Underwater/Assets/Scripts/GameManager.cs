ea46424e66ee8f61522659645510db9bca9c6adb
piperabel <144556881+piperabel@users.noreply.github.com>
2023-10-27 17:05:38 -0600

    First Commit
    - basic fish model
    - starfish model
    - score text (not allowing to drag into script)

---
+++ Group13Underwater/Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance = null;
+  
+
+    // Score Variables
+    public Text scoreVisual;
+    private int playerScore = 0;
+
+    void Awake()
+    {
+        // I think this is for the player, change accordingly
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Start()
+    {
+        UpdateScoreVisual();
+    }
+    // Update the Score Text Object
+    void UpdateScoreVisual()
+    {
+        scoreVisual.text = "Score: " + playerScore.ToString();
+    }
+    // Award n amount of points (pointsAwarded depends on fish type etc.)
+    public void AddScore(int pointsAwarded)
+    {
+        playerScore += pointsAwarded;
+        Debug.Log(playerScore);
+        UpdateScoreVisual();
+    }
+
+
+}
