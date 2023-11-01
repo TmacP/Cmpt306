cec35263f75d82774e574394fd4b4332b7fa0886
Jacob Kendall <jjk256@mail.usask.ca>
2023-11-01 14:26:02 -0600

    Added Temp demo Scene & basic temp player to test power-ups/buffs for collectable items

---
+++ Group13Underwater/Assets/Scripts/JacobTEMPPlayer.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JacobTEMPPlayer : MonoBehaviour
+{
+    /*
+     THIS PLAYER SCRIPT IS A TEMPORARY ONE TO TEST COLLECTABLES
+    IT WILL BE REPLACED WITH THE ACTUAL ONE WHEN MERGING LATER
+    I JUST WANT TO MAKE SURE I CAN GIVE POWER-UPS
+     */
+
+    [SerializeField] private float moveSpeed = 5.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    /*
+     * Temp Basic Movement script
+     */
+    private void Movement() {
+        if (Input.GetKey(KeyCode.W)) transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.S)) transform.Translate(Vector2.down * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A)) transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D)) transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+
+    }
+}
