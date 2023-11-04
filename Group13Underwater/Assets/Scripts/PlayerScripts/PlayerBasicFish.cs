0b339c268a362050fa6e775cd352df6d330c3fed
beatburritochef <beatburritochef@gmail.com>
2023-11-04 15:38:03 -0600

    collect basic fish works

--- Group13Underwater/Assets/Scripts/PlayerScripts/PlayerBasicFish.cs
+++ Group13Underwater/Assets/Scripts/PlayerScripts/PlayerBasicFish.cs
@@ -2,19 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerBasicFish : MonoBehaviour
 {
-
     // When the player contacts a fish, add point and destroy fish
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.transform.tag == "BasicFish")
+        if (other.transform.tag == "BasicFish" && (GameManager.instance != null))
         {
-            // Add a point
-            GameManager.instance.AddScore(1);
+
+                // Add a point
+                GameManager.instance.AddScore(1);
+
+
             // Destroy the fish instance
-            Destroy(this.gameObject);
+            Destroy(other.gameObject);
         }
     }
 }
-
