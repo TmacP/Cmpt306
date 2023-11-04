094faf947d4a7eaa9964e7c5d09f10bd6bf0c0fb
beatburritochef <beatburritochef@gmail.com>
2023-11-04 10:22:20 -0600

    Movespeed buff fixed

--- Group13Underwater/Assets/Scripts/Buffs/MoveSpeedBuff.cs
+++ Group13Underwater/Assets/Scripts/Buffs/MoveSpeedBuff.cs
@@ -1,57 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Powerups/MoveSpeedBuff")]
 public class MoveSpeedBuff : PowerupEffect
 {
-    public float speedMultiplier; // The muliplier to increase speed
-    public float maxBuffDuration; // The max duration of the buff
-    public float remainingBuffDuration; // How much time is left on the buff
+    public float speedMultiplier; 
+    public float maxBuffDuration; 
     private bool isBuffed = false;
+    private bool enableDebugLogs = true; // Control debug logs
 
     public override void Apply(GameObject target)
     {
-        /* Need to get MonoBehaviour so that we can use Coroutines, which are nice
-         * for having Power-ups only last temporarily
-        */
-        MonoBehaviour monoBehaviour = target.GetComponent<MonoBehaviour>();
-        // If the target is NOT buffed, run the Pickup coroutine
-        if (!isBuffed) 
+        PlayerMovement player = target.GetComponent<PlayerMovement>();
+
+        if (player != null && !isBuffed)
         {
-            remainingBuffDuration = maxBuffDuration;
-            monoBehaviour.StartCoroutine(Pickup(target));
+            MonoBehaviour monoBehaviour = target.GetComponent<MonoBehaviour>();
+            monoBehaviour.StartCoroutine(Pickup(player)); // Start the Pickup coroutine
+            if (enableDebugLogs) Debug.Log("Pickup Speed Buff"); // Debug log with comment
         }
-        else
-        {
-            // If the Target IS buffed, refresh the buff duration without altering
-            // the movespeed or anything
-            remainingBuffDuration = maxBuffDuration; 
-        }
-}
-
-IEnumerator Pickup(GameObject target)
-    {
-        Debug.Log("Power-up Picked Up"); // Temp Message
-        
-
-        // Spawn an effect so that the player knows something occured:
-
-        // Apply MoveSpeed Buff
-        PlayerMovement player = target.GetComponent<PlayerMovement>(); // get player data, can change
-        isBuffed = true; // set isBuffed to true
-
-        player.moveSpeed *= speedMultiplier; // increase movement speed by multiplier
-
-        while (remainingBuffDuration > 0f)
-        {
-            yield return null; // Return nothing
-            remainingBuffDuration -= Time.deltaTime; // Decrease the remaining duration
-        }
-        player.moveSpeed /= speedMultiplier; // Revert MS buff
-        isBuffed = false; // Player is no longer buffed
     }
 
+    IEnumerator Pickup(PlayerMovement player)
+    {
+        if (enableDebugLogs) Debug.Log("Power-up Picked Up #DEBUG"); // Debug log with comment
 
+        isBuffed = true;
+        player.moveSpeed *= speedMultiplier;
+
+        float timer = maxBuffDuration;
+        while (timer > 0f)
+        {
+            if (enableDebugLogs) Debug.Log("Remaining Buff Duration: " + timer + " seconds #DEBUG"); // Debug log for remaining duration
+            yield return null;
+            timer -= Time.deltaTime;
+        }
+
+        if (enableDebugLogs) Debug.Log("Buff Expired #DEBUG"); // Debug log for buff expiration
+        player.moveSpeed /= speedMultiplier;
+        isBuffed = false;
+    }
 }
