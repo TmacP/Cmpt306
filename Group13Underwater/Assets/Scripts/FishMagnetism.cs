a706bc3c0536e2ea1feb14a600ae1368911086ca
beatburritochef <beatburritochef@gmail.com>
2023-11-04 11:23:56 -0600

    ScoreCollectable adds score... almost

--- Group13Underwater/Assets/Scripts/FishMagnetism.cs
+++ Group13Underwater/Assets/Scripts/FishMagnetism.cs
@@ -1,27 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
+/// <summary>
+/// This script controls the magnetism of an item toward the player.
+/// This item give the player a score when it is collected.
+/// </summary>
 public class FishMagnetism : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 10.0f;
-
-    /*private void OnTriggerStay(Collider other)
-    {
-        if (other.transform.tag == "Player" && other.GetComponent<JacobTEMPPlayer>().hasMagnetBuff)
-        {
-            //transform.position = Vector3.MoveTowards(transform.position, other.transform.position, moveSpeed * Time.deltaTime);
-            transform.position = Vector2.MoveTowards(transform.position, other.transform.position, moveSpeed * Time.deltaTime);
-        }
-    }*/
+    private bool enableDebugLogs = true; // Control debug logs
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.transform.tag == "Player" && collision.GetComponent<PlayerBuff>().hasMagnetBuff)
+        if (collision.transform.CompareTag("Player") && collision.GetComponent<PlayerBuff>().hasMagnetBuff)
         {
-            //transform.position = Vector3.MoveTowards(transform.position, other.transform.position, moveSpeed * Time.deltaTime);
             transform.position = Vector2.MoveTowards(transform.position, collision.transform.position, moveSpeed * Time.deltaTime);
+            if (enableDebugLogs) Debug.Log("Item moved towards player."); //DEBUG
+            if (Vector2.Distance(transform.position, collision.transform.position) < 0.1f)
+            {
+                AddScoreWhenCollected();
+            }
         }
+    }
+
+    private void AddScoreWhenCollected()
+    {
+        GameManager gameManager = GameManager.instance;
+        if (gameManager != null)
+        {
+            gameManager.AddScore(1);
+            if (enableDebugLogs) Debug.Log("Score added when item was collected."); //DEBUG
+        }
+        Destroy(gameObject);
     }
 }
