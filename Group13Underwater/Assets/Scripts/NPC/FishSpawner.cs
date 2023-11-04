b9a275af05b093392717804b7f2b25b4996f9fb5
diksh <diksha.singh0212@gmail.com>
2023-11-04 15:45:15 -0600

    Updated temporary spawner script with logic

--- Group13Underwater/Assets/Scripts/NPC/FishSpawner.cs
+++ Group13Underwater/Assets/Scripts/NPC/FishSpawner.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class FishSpawner : MonoBehaviour
 {
+    public GameObject Enemy;
+    public int xPos;
+    public int zPos;
+    public int EnemyCount;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnEnemies());
     }
 
     // Update is called once per frame
-    void Update()
+    private IEnumerator SpawnEnemies()
     {
+        while (EnemyCount<20){
+            xPos = Random.Range(-16,13);
+            zPos = Random.Range(-20,1);
+            Instantiate(Enemy, new Vector3(xPos,-4,zPos), Quaternion.identity);
+            yield return new WaitForSeconds(5);
+            EnemyCount+=1;
+        }
         
     }
 }
