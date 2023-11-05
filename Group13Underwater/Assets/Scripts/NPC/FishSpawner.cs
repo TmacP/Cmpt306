4f393b46a2c6e823103bbd1e2273eafe1350dac6
diksh <diksha.singh0212@gmail.com>
2023-11-05 11:46:55 -0600

    Enemy Spawner works

--- Group13Underwater/Assets/Scripts/NPC/FishSpawner.cs
+++ Group13Underwater/Assets/Scripts/NPC/FishSpawner.cs
@@ -1,44 +1,46 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Assertions;
-using static UnityEngine.Random;
 
 public class FishSpawner : MonoBehaviour
 {
-	public GameObject enemyPrefab;
-	[SerializeField] private float spawnRate = 2.0f;
-	[SerializeField] private float spawnRadius = 6f;
-	private float maxVar = 0.05f;
-	//[SerializeField] private Transform[] spawnPoints;
-	private float spawnTimer;
-	private int numberOfEnemiesSpawned = 0;
+    public GameObject fishEnemyPrefab;
+    public Transform playerTransform;
+    public float spawnRadius = 10f;
+    public int maxEnemies = 20;
+    public float spawnInterval = 4f;
 
-	
-   // Update is called once per frame
-    void Start()
+    private int currentEnemyCount = 0;
+    private bool enableSpawning = true;
+
+    private void Start()
     {
-        InvokeRepeating("SpawnEnemy", 0.05f, spawnRate);
+        StartCoroutine(SpawnEnemies());
     }
 
+    private IEnumerator SpawnEnemies()
+    {
+        while (enableSpawning && currentEnemyCount < maxEnemies)
+        {
+            Vector3 randomSpawnPoint = GetRandomSpawnPoint();
+            Instantiate(fishEnemyPrefab, randomSpawnPoint, Quaternion.identity);
+            currentEnemyCount++;
 
-	private void SpawnEnemy(){
-		
-		if(Time.time >spawnTimer){
-			Vector3 playerPosition = GameManager.instance.player.transform.position;
-			float randomAngle = Random.Range(0, 2 * Mathf.PI);
-			 Vector3 randomSpawnOffset = new Vector3(
-                spawnRadius * Mathf.Cos(randomAngle),
-                0,
-                spawnRadius * Mathf.Sin(randomAngle)
-            );
-			Vector3 randomSpawnPosition = playerPosition + randomSpawnOffset;
-			GameObject newEnemy = Instantiate(enemyPrefab, randomSpawnPosition, Quaternion.identity);
-			numberOfEnemiesSpawned++;
-			spawnTimer=Time.time+spawnRate+Random.Range(-maxVar,maxVar);
-		}
+            yield return new WaitForSeconds(spawnInterval);
+        }
+        StopSpawning();
+    }
 
+    private Vector3 GetRandomSpawnPoint()
+    {
+        Vector2 randomCircle = Random.insideUnitCircle.normalized * spawnRadius;
+        Vector3 spawnPoint = new Vector3(randomCircle.x, 0f, randomCircle.y) + playerTransform.position;
+        spawnPoint.y = 0f; // Ensure enemies spawn at ground level or appropriate height in your game
+
+        return spawnPoint;
+    }
+
+    public void StopSpawning()
+    {
+        enableSpawning = false;
+    }
 }
-}
-
-
