2c784632ab94de5d7a14ef5a5cb3199d902ff566
diksh <diksha.singh0212@gmail.com>
2023-11-04 20:54:14 -0600

    Trying to spawn enemy at certain empty positions

--- Group13Underwater/Assets/Scripts/NPC/FishSpawner.cs
+++ Group13Underwater/Assets/Scripts/NPC/FishSpawner.cs
@@ -8,6 +8,8 @@ public class FishSpawner : MonoBehaviour
     public int xPos;
     public int zPos;
     public int EnemyCount;
+    public float terrainHeightOffset = 2f;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -21,9 +23,21 @@ public class FishSpawner : MonoBehaviour
         while (EnemyCount<20){
             xPos = Random.Range(-16,13);
             zPos = Random.Range(-20,1);
-            Instantiate(Enemy, new Vector3(xPos,-4,zPos), Quaternion.identity);
-            yield return new WaitForSeconds(5);
+            
+            // Raycast to find the terrain height at the spawn point
+            RaycastHit hit;
+
+            Vector3 raycastOrigin = new Vector3(xPos, 100f, zPos); // Cast ray from above to find terrain
+            if (Physics.Raycast(raycastOrigin, Vector3.down, out hit, Mathf.Infinity, LayerMask.GetMask("TileMap")))
+            {  float yPosition = hit.point.y + terrainHeightOffset;
+                Vector3 spawnPosition = new Vector3(xPos, yPosition, zPos);
+                Instantiate(Enemy, spawnPosition, Quaternion.identity);
+
+            //Instantiate(Enemy, new Vector3(xPos,-4,zPos), Quaternion.identity);
+            }
+             yield return new WaitForSeconds(8);
             EnemyCount+=1;
+
         }
         
     }
