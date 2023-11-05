e6065dbd700b75cde1191aae654a6e63b9d14006
LindseyER <100873359+LindseyER@users.noreply.github.com>
2023-11-05 15:29:01 -0600

    Putting healBuff fix into MainScene

--- Group13Underwater/Assets/Scripts/Buffs/HealBuff.cs
+++ Group13Underwater/Assets/Scripts/Buffs/HealBuff.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Powerups/HealBuff")]
 public class HealBuff : PowerupEffect
 {
-    private bool enableDebugLogs = true; // Control debug logs
+    private bool enableDebugLogs = false; // Control debug logs
 
     [SerializeField] public float healAmount; //currently stuck at 50 for some reason LR Nov 5
     public override void Apply(GameObject target)
@@ -17,14 +17,18 @@ public class HealBuff : PowerupEffect
             if ((target.GetComponent<PlayerHealth>().playerHealth + healAmount) > 100.0f)
             {
                 target.GetComponent<PlayerHealth>().playerHealth = 100.0f;
+                if (enableDebugLogs) { Debug.Log("Health after heal:" + target.GetComponent<PlayerHealth>().playerHealth); } //DEBUG
             }
             else
             {
                 target.GetComponent<PlayerHealth>().playerHealth += healAmount;
+                if (enableDebugLogs) { Debug.Log("Health after heal:" + target.GetComponent<PlayerHealth>().playerHealth); } //DEBUG
+
             }
         }
-
-        if (enableDebugLogs) { Debug.Log("Health after heal:" + target.GetComponent<PlayerHealth>().playerHealth); } //DEBUG
-
+        else
+        {
+            if (enableDebugLogs) { Debug.Log("Health after heal:" + target.GetComponent<PlayerHealth>().playerHealth); } //DEBUG
+        }
     }
 }
