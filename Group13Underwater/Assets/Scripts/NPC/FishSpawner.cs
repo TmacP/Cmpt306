746b212e51f8da8151641155e351c70b1df29a4a
diksh <diksha.singh0212@gmail.com>
2023-11-05 12:35:45 -0600

    spawner work for enemies

--- Group13Underwater/Assets/Scripts/NPC/FishSpawner.cs
+++ Group13Underwater/Assets/Scripts/NPC/FishSpawner.cs
@@ -7,7 +7,7 @@ public class FishSpawner : MonoBehaviour
     public Transform playerTransform;
     public float spawnRadius = 10f;
     public int maxEnemies = 20;
-    public float spawnInterval = 4f;
+    public float spawnInterval = 8f;
 
     private int currentEnemyCount = 0;
     private bool enableSpawning = true;
@@ -34,7 +34,7 @@ public class FishSpawner : MonoBehaviour
     {
         Vector2 randomCircle = Random.insideUnitCircle.normalized * spawnRadius;
         Vector3 spawnPoint = new Vector3(randomCircle.x, 0f, randomCircle.y) + playerTransform.position;
-        spawnPoint.y = 0f; // Ensure enemies spawn at ground level or appropriate height in your game
+        spawnPoint.y = 0f; // Ensure enemies spawn at ground level 
 
         return spawnPoint;
     }
