a19e6ffc7b0a6383c8f1e8ba5c8dd114aa5accb9
beatburritochef <beatburritochef@gmail.com>
2023-11-13 22:19:35 -0600

    player speed up when on last heart

--- Group13Underwater/Assets/Scripts/Buffs/HealBuff.cs
+++ Group13Underwater/Assets/Scripts/Buffs/HealBuff.cs
@@ -10,25 +10,34 @@ public class HealBuff : PowerupEffect
     [SerializeField] public float healAmount; //currently stuck at 50 for some reason LR Nov 5
     public override void Apply(GameObject target)
     {
-        if (enableDebugLogs) { Debug.Log("Health before heal:" + target.GetComponent<PlayerHealth>().playerHealth); } //DEBUG
+        PlayerHealth playerHealth = target.GetComponent<PlayerHealth>();
+        PlayerMovement playerMovement = target.GetComponent<PlayerMovement>();
 
-        if (target.GetComponent<PlayerHealth>().playerHealth < 100.0f)
+        if (playerHealth != null && playerMovement != null)
         {
-            if ((target.GetComponent<PlayerHealth>().playerHealth + healAmount) > 100.0f)
+            if (enableDebugLogs) { Debug.Log("Health before heal:" + playerHealth.playerHealth); } // DEBUG
+
+            if (playerHealth.playerHealth < 100.0f)
             {
-                target.GetComponent<PlayerHealth>().playerHealth = 100.0f;
-                if (enableDebugLogs) { Debug.Log("Health after heal:" + target.GetComponent<PlayerHealth>().playerHealth); } //DEBUG
+                if ((playerHealth.playerHealth + healAmount) > 100.0f)
+                {
+                    playerHealth.playerHealth = 100.0f;
+                    if (enableDebugLogs) { Debug.Log("Health after heal:" + playerHealth.playerHealth); } // DEBUG
+                }
+                else
+                {
+                    playerHealth.playerHealth += healAmount;
+                    if (enableDebugLogs) { Debug.Log("Health after heal:" + playerHealth.playerHealth); } // DEBUG
+                }
             }
             else
             {
-                target.GetComponent<PlayerHealth>().playerHealth += healAmount;
-                if (enableDebugLogs) { Debug.Log("Health after heal:" + target.GetComponent<PlayerHealth>().playerHealth); } //DEBUG
-
+                if (enableDebugLogs) { Debug.Log("Health after heal:" + playerHealth.playerHealth); } // DEBUG
             }
-        }
-        else
-        {
-            if (enableDebugLogs) { Debug.Log("Health after heal:" + target.GetComponent<PlayerHealth>().playerHealth); } //DEBUG
+
+            // Set the player's movement speed to 1000
+            // MAGIC NUMBER ALERT: 1000 is the default movement speed. tm to handle tricky edge case where player heals last heart
+            playerMovement.moveSpeed = 1000;
         }
     }
 }
