ebe4b0a41e4ed454192d1c619328c42ad1254941
piperabel <144556881+piperabel@users.noreply.github.com>
2023-11-15 14:27:02 -0600

    message in bottle

---
+++ Group13Underwater/Assets/Scripts/PlayerScripts/PlayerBottle.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBottle : MonoBehaviour
+{
+    public GameObject messageScreen;
+    public GameObject bottle;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.tag == "MessageBottle" && (GameManager.instance != null))
+        {
+            // Destroy the bottle 
+            Destroy(other.gameObject);
+
+            // Show the message (pause game, show message panel)
+            Time.timeScale = 0;
+            messageScreen.SetActive(true);
+
+            // Close message with button
+
+        }
+    }
+}
