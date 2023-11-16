352f3cdb84d6ed03fa27e2f6d135b9e89cece74c
piperabel <144556881+piperabel@users.noreply.github.com>
2023-11-16 00:48:16 -0600

    Fixes
    -Fixed uncommon fish spawning
    -Fixed bottle spawning
    -Having issues with adding money (no error but doesnt add)
    -Having issues with messages only randomly generating once per start instead of per spawn

--- Group13Underwater/Assets/Scripts/PlayerScripts/PlayerBottle.cs
+++ Group13Underwater/Assets/Scripts/PlayerScripts/PlayerBottle.cs
@@ -1,11 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor.VersionControl;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerBottle : MonoBehaviour
 {
-    //public GameObject messageScreen;
+    public GameObject messageScreen;
     public GameObject bottle;
+
+    public TextMeshProUGUI bottleMessage; // This is the message that appears in the scroll pop up
+    string[] messages = new string[] { "To the one who finds this bottle, be shore of yourself! I put a few dollars in here to support your journey.", "May this message find its way to the one who needs it. Seas the day! Here’s five dollars from yours truly.", "Dear reader, you are doing fin-tastic! Buy yourself something nice with the money I left.", "May this bottle reach you at a time in need. Don’t be crabby, heres some money to bring your spirits up." };
+
+    void Start()
+    {
+        // this should change the text of the message
+        messagePicker();
+    }
+    private void Update()
+    {
+       
+    }
+
+    public void messagePicker()
+    {
+        // pick one of the messages
+        string RandomWord = messages[Random.Range(0, messages.Length)];
+        bottleMessage.text = RandomWord;
+    }
+
+    // Button function for the user to close the screen
+    public void CloseMessage()
+    {
+        // Set Active False?
+        messageScreen.SetActive(false);
+        Time.timeScale = 1;
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.transform.tag == "MessageBottle" && (GameManager.instance != null))
@@ -17,7 +48,7 @@ public class PlayerBottle : MonoBehaviour
 
             // Show the message (pause game, show message panel)
             Time.timeScale = 0;
-           // messageScreen.SetActive(true);
+            messageScreen.SetActive(true);
 
             // Close message with button in message bottle script
 
--- Group13Underwater/Assets/Scripts/UI/MessageBottle.cs
+++ Group13Underwater/Assets/Scripts/UI/MessageBottle.cs
@@ -6,8 +6,9 @@ using UnityEngine.UI;
 
 public class MessageBottle : MonoBehaviour
 {
- 
+
     public GameObject messageScreen;
+    public GameObject playerPref;
     public TextMeshProUGUI bottleMessage; // This is the message that appears in the scroll pop up
     string[] messages = new string[] { "To the one who finds this bottle, be shore of yourself! I put a few dollars in here to support your journey.", "May this message find its way to the one who needs it. Seas the day! Here’s five dollars from yours truly.", "Dear reader, you are doing fin-tastic! Buy yourself something nice with the money I left.", "May this bottle reach you at a time in need. Don’t be crabby, heres some money to bring your spirits up." };
 
@@ -21,16 +22,16 @@ public class MessageBottle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     // Randomly pick a message from an array
     public void messagePicker()
     {
         // pick one of the messages
-        string RandomWord= messages[Random.Range(0, messages.Length)];
+        string RandomWord = messages[Random.Range(0, messages.Length)];
         bottleMessage.text = RandomWord;
     }
-    
+
     // Button function for the user to close the screen
     public void CloseMessage()
     {
@@ -38,6 +39,23 @@ public class MessageBottle : MonoBehaviour
         messageScreen.SetActive(false);
         Time.timeScale = 1;
     }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.tag == "Player" && (GameManager.instance != null))
+        {
+            // Add 5 money
+            //GameManager.instance.AddMoney(5);
+            // Destroy the bottle 
+            //Destroy(other.gameObject);
+
+            // Show the message (pause game, show message panel)
+            Time.timeScale = 0;
+            messageScreen.SetActive(true);
+
+            
+
+        }
 
 
+    }
 }
