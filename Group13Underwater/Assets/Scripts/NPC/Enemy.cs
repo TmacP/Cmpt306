39585af53a58ceba8deb28d9e99b20a3b4ca3375
diksh <diksha.singh0212@gmail.com>
2023-11-17 16:58:26 -0600

    Player hits the enemy do not think fov is working but I will do one commit in case I want to come back to it

--- Group13Underwater/Assets/Scripts/NPC/Enemy.cs
+++ Group13Underwater/Assets/Scripts/NPC/Enemy.cs
@@ -4,38 +4,60 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Default Enemy stats
     [SerializeField] private float moveSpeed = 15.0f;
     [SerializeField] private float health = 100.0f;
-
     [SerializeField] private float damageToPlayer = 10.0f;
     [SerializeField] private float damageRate = 0.2f;
     private float damageTime;
 
-    private PlayerHealth playerHealth; // Reference to the PlayerHealth script.
+    private PlayerHealth playerHealth;
+    private bool enableDebugLogs = false;
 
-    private bool enableDebugLogs = false; // Flag to enable or disable debug logs.
+    [SerializeField] private float fieldOfViewAngle = 90f;
+    [SerializeField] private float visionRange = 20f;
 
     private void Start()
     {
         playerHealth = GameManager.instance.player.GetComponent<PlayerHealth>();
-        if (enableDebugLogs){Debug.Log("Enemy Start");}//DEBUG
-        // Find the PlayerHealth script attached to the player GameObject.
+        if (enableDebugLogs) { Debug.Log("Enemy Start"); } // DEBUG
     }
 
     private void FixedUpdate()
     {
-        Movement();
+        // Check if the player is within the field of view
+        if (IsPlayerInFieldOfView())
+        {
+            // Move towards the player
+            MoveTowardsPlayer();
+        }
     }
 
-    private void Movement()
+    private bool IsPlayerInFieldOfView()
     {
-        if (GameManager.instance.player)
-        {
-            Vector3 targetPosition = GameManager.instance.player.transform.position;
-            Vector3 moveDirection = (targetPosition - transform.position).normalized;
-            transform.position += moveDirection * moveSpeed * Time.deltaTime;
-        }
+        //if (GameManager.instance.player)
+        //{
+            Vector3 directionToPlayer = GameManager.instance.player.transform.position - transform.position;
+            float angleToPlayer = Vector3.Angle(transform.up, directionToPlayer);
+
+            if (angleToPlayer < fieldOfViewAngle * 0.5f)
+            {
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, directionToPlayer.normalized, visionRange);
+
+                if (hit.collider != null && hit.collider.CompareTag("Player"))
+                {
+                    return true;
+                }
+            }
+        //}
+
+        return false;
+    }
+
+    private void MoveTowardsPlayer()
+    {
+        Vector3 targetPosition = GameManager.instance.player.transform.position;
+        Vector3 moveDirection = (targetPosition - transform.position).normalized;
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
     }
 
     public void TakeDamage(float damage)
@@ -50,10 +72,10 @@ public class Enemy : MonoBehaviour
 
     private void OnTriggerStay2D(Collider2D other)
     {
-        if (enableDebugLogs){Debug.Log("On trigger stay");}// DEBUG
+        if (enableDebugLogs) { Debug.Log("On trigger stay"); } // DEBUG
         if (other.transform.tag == "Player" && Time.time > damageTime && other is CapsuleCollider2D)
         {
-            if (enableDebugLogs){Debug.Log("Enemy is colliding with " + other.transform.tag);}
+            if (enableDebugLogs) { Debug.Log("Enemy is colliding with " + other.transform.tag); }
 
             if (playerHealth != null)
             {
