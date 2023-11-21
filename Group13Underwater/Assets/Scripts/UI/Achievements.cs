4ef458625ef4ed27900fd86eb0cedf1a29829b54
Tristen MacPherson <trm003@mail.usask.ca>
2023-11-21 15:28:44 -0600

    Implement achievements

--- Group13Underwater/Assets/Scripts/UI/Achievements.cs
+++ Group13Underwater/Assets/Scripts/UI/Achievements.cs
@@ -1,57 +1,45 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Achievements : MonoBehaviour
 {
+    public Canvas achievementCanvas;
+    public Image badgeImage;
 
-    private List<string> achievement_list = new List<string>();
-    private int kills = 0;
-    [SerializeField] private int kills_to_next = 5;
-    private int kills_next_achievement = 0;
-
-
-    private int scolls_collected = 0;
-    [SerializeField] private int scrolls_to_next = 1;
-    private int scrolls_next_achievement = 0;
+    public Sprite[] badgeSprites; // Add your badge sprites in the Inspector
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        achievementCanvas.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (kills == kills_next_achievement) {
-
-            KillsNextAchievement();
-            kills_next_achievement += 1; 
-
-        }
-
-        if (scolls_collected == scrolls_to_next) {
-
-            ScrollsNextAchievement();
-            scrolls_next_achievement += 1;
+        // Simulating the player achieving something
+        if (Input.GetKeyDown(KeyCode.Space)) // Replace this with your actual achievement logic
+        {
+            ShowAchievement();
         }
     }
 
-    void KillsNextAchievement() {
-    
-        achievement_list.Add(string.Format("Defeated {0} Enemies", kills_to_next));
-        // place holder for canvas element code
-        kills_to_next *= 2;
-    
+    void ShowAchievement()
+    {
+        int randomIndex = Random.Range(0, badgeSprites.Length);
+        Sprite randomBadge = badgeSprites[randomIndex];
+
+        StartCoroutine(ShowAndFadeAchievement(randomBadge));
     }
 
-        void ScrollsNextAchievement() {
+    IEnumerator ShowAndFadeAchievement(Sprite badgeSprite)
+    {
+        badgeImage.sprite = badgeSprite;
+        achievementCanvas.enabled = true;
 
-        achievement_list.Add(string.Format("You collected {0}", scrolls_to_next));
-        // place holder for canvas element code
-        scrolls_to_next += 1;
+        yield return new WaitForSeconds(5f); // Show the badge for 5 seconds
 
+        achievementCanvas.enabled = false;
     }
-    
 }
