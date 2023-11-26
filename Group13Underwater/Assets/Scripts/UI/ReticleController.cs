e62086ad2806b0088fb1ca818ad1de0576399e80
beatburritochef <beatburritochef@gmail.com>
2023-11-26 15:10:10 -0600

    reticle

---
+++ Group13Underwater/Assets/Scripts/UI/ReticleController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ReticleController : MonoBehaviour
+{
+    // Adjust the speed of reticle movement
+
+void Awake()
+{
+    Cursor.visible = false;
+}
+
+void Update()
+{
+    // Get the mouse position
+    Vector2 mousePosition = Input.mousePosition;
+    // Set the reticle position to the mouse position
+    transform.position = mousePosition;
+}
+}
