749687c416bde61fdc5340e4221e429d76727e32
beatburritochef <beatburritochef@gmail.com>
2023-11-26 19:18:05 -0600

    seems to work

--- Group13Underwater/Assets/Scripts/UI/ReticleController.cs
+++ Group13Underwater/Assets/Scripts/UI/ReticleController.cs
@@ -2,18 +2,16 @@ using UnityEngine;
 
 public class ReticleController : MonoBehaviour
 {
-    // Adjust the speed of reticle movement
+    public SkinSelectMenu skinSelectMenu; // Reference to the SkinSelectMenu script or object
 
-void Awake() 
-{
-    Cursor.visible = false;
-}
-
-void Update()
-{
-    // Get the mouse position
-    Vector2 mousePosition = Input.mousePosition;
-    // Set the reticle position to the mouse position
-    transform.position = mousePosition;
-}
+    void Update()
+    {
+        if (!skinSelectMenu.skinselectmenu.activeSelf)
+        {
+            // Get the mouse position
+            Vector2 mousePosition = Input.mousePosition;
+            // Set the reticle position to the mouse position
+            transform.position = mousePosition;
+        }
+    }
 }
